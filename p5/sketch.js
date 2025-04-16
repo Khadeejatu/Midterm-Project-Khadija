ae995f91b9666f691e5b01629895dc0ac829eb1b
Khadeejatu <Kd2720@nyu.edu>
2025-04-16 13:55:22 -0400

    push

---
+++ p5/sketch.js
@@ -0,0 +1,33 @@
+
+//-------------------------------------------------------------------------------
+function setup() 
+ {
+  createCanvas(windowWidth, windowHeight);
+  background(255,0,0);
+print();
+console.log();
+  
+ }
+ //-----------------------------------------------------------------------------------
+
+ function draw() 
+ {
+
+
+ }
+  
+//------------------------------------------------------------------------------------
+ function mousePressed()
+ 
+ { 
+  print("mouseX is: ");
+  print(mouseX);
+  print(", mouseY is: ");
+  print(mouseY);
+  print (timing);
+
+ }
+ //--------------------------------------------------------------------
+//My separate functions
+
+ 
